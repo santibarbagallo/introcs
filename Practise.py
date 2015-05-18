2da4d5d80ec4990897fe0522c8f5db267d6b5837
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-18 09:58:27 -0500

    Create Practise.py

---
+++ Practise.py
@@ -0,0 +1,19 @@
+#Practise Homework
+Name = "Santiago,"
+Last = "Barbagallo,"
+Addres = "La casa Presidencial,"
+Email = "18sbarbagallo@iaa.edu.ec,"
+Phone = "+593 99 0605060,"
+
+#Printing individualy for each variable
+print Name
+print Last
+print Addres
+print Email
+print Phone
+
+print #this creates a space between each print
+print #this creates a space between each print
+
+#Printing variables all together in one line
+print "My name is", Name, "and my last name is", Last, "I live in", Addres, "my email is", Email, "and my phone number is", Phone, "Thanks"
