381997bbcc7507182a2835e1e8844774081b48bc
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-18 09:56:47 -0500

    Create CS.py

---
+++ CS.py
@@ -0,0 +1,17 @@
+mystring="hello" #string
+myfloat=10 #floating point
+myint=20 #integer
+print 
+#Printing on Screen
+print mystring
+print myfloat
+print myint
+print 
+print mystring, "I am,", myfloat," I will be", myint, "in ten years."
+print 
+#Mathematical operations
+print (4+8)*19/6
+print 65*9/3
+print 89*1999/9 
+print 500/8*2 
+print 88817/1919*5
