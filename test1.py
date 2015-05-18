40550413ce0e34ebf003fcd51beee7a67426c5f4
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-18 10:00:53 -0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,17 @@
+operand1=95
+operand2=64.5
+
+print operand1+operand2
+print operand1-operand2
+print operand1*operand2
+print operand1/operand2
+print operand1%operand2
+
+#if the value of operand 1 is changed, then only the value of the variable has to change,
+#since the problems have the variable and not a certain number.
+
+#this makes the procedure and work much easier, since you dont 
+#have to rewrite everyhting if you want to change somehting.
+
+#i got the solution by using both variables in different equations,
+#and then from there you will get the answer.
