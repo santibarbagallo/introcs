200d676aa5e1567930b3dc1d7cfacf8c79506e70
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-18 09:56:20 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+print "Hello World"
+print "My Name is Santi" 
+print "I am 14 years old \nThis is Phyton"
+
+print 2015+1 #Printing in integer
+print "2015+1"
+
+print 3.146 #Printing a floatign point
+
+print 8+19 #adding numbers
+print 9-3 #substracting numbers
+print 9*5 #multipying numbers
+print 81/9 #dividing numbers
+
+#modulus
+print 10%3
+
+print "odd or even numbers"
+print 17%2
+print 16%2
