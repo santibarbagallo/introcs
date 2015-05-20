260c72e086770f35ade0e7f9a564da2a7a5f4a93
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-20 10:41:04 -0500

    Create Practise1.py

---
+++ Practise1.py
@@ -0,0 +1,17 @@
+name = raw_input("yout name is?")
+last = raw_input("your last name is?")
+birth = input("when where you born?")
+singer = raw_input("what is your favorite band?")
+movie = raw_input("what is your favorite movie?")
+bff = raw_input("who is your best friend?")
+
+
+print "my name is", name
+print "my last name is", last 
+print "i was born in", birth
+print "my favorite singer is", singer
+print "my favorite movie is", movie 
+print "my best friend is", bff 
+print "i am currently", 2015-birth, "years old"
+
+print name*10
