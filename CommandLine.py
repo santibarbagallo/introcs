7a5832c7edf03d04797c938a727ed55f1ec45c2b
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-20 09:27:22 -0500

    Create CommandLine.py

---
+++ CommandLine.py
@@ -0,0 +1,13 @@
+# name = "Gertrudis"
+# print "My name is", name
+
+import sys
+print "your name is: ", sys.argv[1]
+print "your middle name is: ", sys.argv[2]
+print "your last name is: ", sys.argv[3]
+
+print "your name is: ", sys.argv[1]
+print "your middle name is: ", sys.argv[2]
+print "I was born in: ", sys.argv[4]
+print "I graduate in: ", sys.argv[5]
+print "My gpa is: ", sys.argv[6]
