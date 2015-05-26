06f5b195bf952b151c407b994db79cf0b4f5039a
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-26 10:17:51 -0500

    Create Conditions.py

---
+++ Conditions.py
@@ -0,0 +1,33 @@
+#conditional keywoards
+#If (argument)
+    #action
+#Else 
+    #action
+
+#If (argument1)
+    #action
+#Elif (argument1
+    #action
+#Elif (argument1
+    #action
+#Else
+    #action
+    
+#Operators for conditions
+#Less than : <
+#Greater than : >
+#Equal : ==
+#Not equal : !=
+#Less than or equal to : <=
+#Greater than or equal to : >=
+
+#Example
+print "Bar"
+age = input("What is the age of the customer?")
+
+if age >= 18:
+    print  "The customer is legaly allowed to drink"
+    print  "you may serve"
+else: 
+    print "customer is not legally allowed to drink"
+
