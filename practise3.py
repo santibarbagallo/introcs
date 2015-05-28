63591f66c51d591776dec472d691c66457aec38d
santibarbagallo <18sbarbagallo@iaa.edu.ec>
2015-05-28 09:57:31 -0500

    Update practise3.py

--- practise3.py
+++ practise3.py
@@ -1,5 +1,13 @@
 n=input ("give a number")
     
+if n%2 == 0:
+    print "even"
+else:
+    print "odd"
+    
+#another way is 
+n=input ("give a number")
+    
 if (-1)**n==1:
     print "even"
 else:
